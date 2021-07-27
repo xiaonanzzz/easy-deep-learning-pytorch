9b4bc0c2eeebcb5bf1711584a5472bcf5561df03
Xiaonan Zhao <zhaoxxn@gmail.com>
2021-07-26 17:44:30 -0700

    Add resnet 50 embedder

--- easydl/example/proxy_anchor_loss_cub.py
+++ easydl/example/proxy_anchor_loss_cub.py
@@ -1,11 +1,7 @@
-import pandas as pd
 import torch
-import numpy as np
-from torchvision.transforms import ToTensor, Normalize, Compose
 from easydl.datasets.cub import Cub2011MetricLearningDS
 from easydl.trainer.metric_learning import ProxyAnchorLossEmbeddingModelTrainer, EpochEndEvaluationHook
 from torch import nn
-from easydl.evaluator.classification import ClassificationModelEvaluator
 from easydl.models import L2Normalization
 
 class SimpleNet(nn.Module):
@@ -30,7 +26,7 @@ class SimpleNet(nn.Module):
         x = self.fc(x)
         return x
 
-def main():
+def simple_net_example():
     import os
     print('working directory', os.getcwd())
 
@@ -45,6 +41,22 @@ def main():
     trainer.epoch_end_hook = EpochEndEvaluationHook(model, test_data)
     trainer.train()
 
+def resnet50_example():
+    import os
+    from easydl.models.cnn_embedder import Resnet50PALVersion
+    print('working directory', os.getcwd())
+
+    train_data = Cub2011MetricLearningDS(os.path.expanduser('~/data/CUB_200_2011'), split='train')
+    test_data = Cub2011MetricLearningDS(os.path.expanduser('~/data/CUB_200_2011'), split='test')
+
+    print('data shape', train_data[0][0].shape, train_data[0])
+    embsize = 32
+    model = Resnet50PALVersion(embsize)
+
+    trainer = ProxyAnchorLossEmbeddingModelTrainer(model, train_data, embsize, 100, tqdm_disable=False)
+    trainer.epoch_end_hook = EpochEndEvaluationHook(model, test_data)
+    trainer.train()
+
 if __name__ == '__main__':
-    main()
+    resnet50_example()
 

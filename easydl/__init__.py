8e97a0df3c7c329a40efda614c5d9032e8036298
xiaonan zhao <zhaoxxn@gmail.com>
2021-11-21 00:05:25 -0800

    V1.7 alpha + resnet 18 for small image (cifar)

--- easydl/__init__.py
+++ easydl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 1.6
+__version__ = 1.7
 
 from .trainer import *
 from .utils import *
--- easydl/example/cifar.py
+++ easydl/example/cifar.py
@@ -85,8 +85,49 @@ def train_resnet_18():
     print('training code version', easydl.__version__)
     train_image_classification_model_2021_nov(model, train_ds, train_cfg, run_cfg, metric_logger, test_ds=test_ds)
 
+def get_model(model_name):
+    if model_name == 'resnet18-si':
+        from easydl.models.resnet_small_image import ResNet18
+        return ResNet18(10)
+
+    if model_name == 'resnet18':
+        from torchvision.models import resnet18
+        model = resnet18(pretrained=False, num_classes=10)
+        return model
+
+
+def train_cifar():
+    # get configurations from cmd
+    project_name = get_config_from_cmd('project_name', 'cifar_10')
+    wandb_key = get_config_from_cmd('wandb_key', None, key_type=str)
+    data_dir = get_config_from_cmd('data_dir', '~/pytorch_data')
+    model_name = get_config_from_cmd('model', 'resnet18-si')
+    tags = get_config_from_cmd('tags', '').split(',')
+    tags.append('ver-{}'.format(easydl.__version__))
+    tags.append(model_name)
+
+    train_ds = CIFAR10(data_dir, train=True, download=True, transform=transform_train)
+    test_ds = CIFAR10(data_dir, train=False, download=True, transform=transform_test)
+
+    train_cfg = TrainingConfig(optimizer='sgd', lr=0.01, weight_decay=5e-4, lr_scheduler_type='cosine',
+                               train_epoch=100, train_batch_size=128)
+    train_cfg.update_values_from_cmd()
+    run_cfg = RuntimeConfig()
+
+    metric_logger = prepare_logger(wandb_key=wandb_key,
+                                   project_name=project_name,
+                                   tags=tags,    # modify this accordingly !!!
+                                   )
+    metric_logger.update_config(train_cfg.__dict__)
+
+    print('train data shape', train_ds[0][0].shape, str(train_ds[0][0])[:50])
+    print('training code version', easydl.__version__)
+    model = get_model(model_name)
+    train_image_classification_model_2021_nov(model, train_ds, train_cfg, run_cfg, metric_logger, test_ds=test_ds)
+
+
 if __name__ == '__main__':
-    func_name = get_config_from_cmd('function', 'train_resnet_18')
+    func_name = get_config_from_cmd('function', 'train_cifar')
     # run function
     globals()[func_name]()
 
